9130962d0d2c3b417f744a5d644585ddaec51711
VoidRay-X <anmolkm18@gmail.com>
2025-11-16 18:50:42 +0530

    Update Business_Overview.py

--- pages/Business_Overview.py
+++ pages/Business_Overview.py
@@ -12,11 +12,11 @@ website_sessions, website_pageviews, products, orders, order_items, order_item_r
 
 total_session=len(website_sessions['website_session_id'])
 total_order=len(orders['order_id'])
-total_revenue=orders['price_usd'].sum()
+total_revenue=orders['price_usd'].sum()/1000000
 
 col1, col2, col3 = st.columns(3)
 col1.metric("Total Sessions", total_session)
 col2.metric('Total Orders',total_order)
-col3.metric('Total Revenue', total_revenue)
+col3.metric('Total Revenue', total_revenue,'M')
 #col1, col2=st.columns(2)
 #col1.metric('Total Revenue', total_revenue)
