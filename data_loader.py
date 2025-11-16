21a0ef453d712896bc783267c01d50cc2b3241dd
VoidRay-X <anmolkm18@gmail.com>
2025-11-16 17:36:04 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,18 +1,20 @@
-# ------------------------------
-# LOAD DATA FROM GOOGLE DRIVE
-# ------------------------------
-url_sessions = "https://drive.google.com/uc?export=download&id=1ycYab0CgibgaGxopEiZFOCE13mGTm77x"
-url_pageviews = "https://drive.google.com/uc?export=download&id=1Ruc6Q622uJoeDR_pVhKI5hU2rlgTlicy"
-url_products = "https://drive.google.com/uc?export=download&id=1F13iyzdpDmy6j_w_eYaFRmVDfyy3jV0R"
-url_orders = "https://drive.google.com/uc?export=download&id=1csibm0W8Ewphbu3OEaayqbiLQka9qlrm"
-url_order_items = "https://drive.google.com/uc?export=download&id=163b-CGyWRafz-1ZRT0XNepi70MyRzg7O"
-url_order_items_refunds = "https://drive.google.com/uc?export=download&id=1di14Gd4eNh5norDomih8G7Gw57Kz358L"
+import pandas as pd
+import streamlit as st
 
-website_sessions = pd.read_csv(url_sessions)
-website_pageviews = pd.read_csv(url_pageviews)
-products = pd.read_csv(url_products)
-orders = pd.read_csv(url_orders)
-order_items = pd.read_csv(url_order_items)
-order_item_refunds = pd.read_csv(url_order_items_refunds)
+@st.cache_data
+def load_data():
+    url_sessions = "https://drive.google.com/uc?export=download&id=1ycYab0CgibgaGxopEiZFOCE13mGTm77x"
+    url_pageviews = "https://drive.google.com/uc?export=download&id=1Ruc6Q622uJoeDR_pVhKI5hU2rlgTlicy"
+    url_products = "https://drive.google.com/uc?export=download&id=1F13iyzdpDmy6j_w_eYaFRmVDfyy3jV0R"
+    url_orders  = "https://drive.google.com/uc?export=download&id=1csibm0W8Ewphbu3OEaayqbiLQka9qlrm"
+    url_items   = "https://drive.google.com/uc?export=download&id=163b-CGyWRafz-1ZRT0XNepi70MyRzg7O"
+    url_refunds = "https://drive.google.com/uc?export=download&id=1di14Gd4eNh5norDomih8G7Gw57Kz358L"
 
-return website_sessions, website_pageviews, products, orders, order_items, order_item_refunds
+    website_sessions = pd.read_csv(url_sessions)
+    website_pageviews = pd.read_csv(url_pageviews)
+    products = pd.read_csv(url_products)
+    orders = pd.read_csv(url_orders)
+    order_items = pd.read_csv(url_items)
+    order_item_refunds = pd.read_csv(url_refunds)
+
+    return website_sessions, website_pageviews, products, orders, order_items, order_item_refunds
