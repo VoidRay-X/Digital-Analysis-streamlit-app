b05cb3fb0b00f0f9ee4131818fea9c3398a5f020
VoidRay-X <anmolkm18@gmail.com>
2025-11-16 14:44:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-pip install streamlit pandas plotly
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
 st.title("My First Streamlit App!")
 st.write("Hello from Streamlit running on the mee!")
