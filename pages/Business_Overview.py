23625d627c3844012a7633a9f4504c1c6d66c88b
VoidRay-X <anmolkm18@gmail.com>
2025-11-16 18:52:41 +0530

    Update Business_Overview.py

--- pages/Business_Overview.py
+++ pages/Business_Overview.py
@@ -20,3 +20,30 @@ col2.metric('Total Orders',total_order)
 col3.metric('Total Revenue', total_revenue,'M')
 #col1, col2=st.columns(2)
 #col1.metric('Total Revenue', total_revenue)
+
+
+
+sess = website_sessions.groupby('utm_source')['website_session_id'].count().reset_index()
+
+total= sess['website_session_id'].sum()
+sess['percentage'] = (sess['website_session_id'] / total) * 100
+
+# --- Pie Chart ---
+plt.figure(figsize=(7, 7))
+
+# Create pie chart
+plt.pie(
+    sess['percentage'], 
+    labels=sess['utm_source'],
+    autopct='%1.1f%%',        # show one decimal place
+    startangle=90,            # rotate so the first slice starts at the top
+    colors=plt.cm.Set2.colors, # use same Set2 palette
+    wedgeprops={'edgecolor': 'white'} # cleaner edges
+)
+
+# Add title
+plt.title('Percentage of Source-wise Traffic volume', fontsize=14)
+plt.tight_layout()
+
+# Show chart
+plt.show()
