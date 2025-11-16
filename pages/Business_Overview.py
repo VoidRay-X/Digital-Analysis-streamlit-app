f85163ed000eebf01f46b1749df9980465dcfa59
VoidRay-X <anmolkm18@gmail.com>
2025-11-16 18:41:28 +0530

    Update Business_Overview.py

--- pages/Business_Overview.py
+++ pages/Business_Overview.py
@@ -9,7 +9,7 @@ st.title("📈 Business Overview")
 # ----------------------------
 website_sessions, website_pageviews, products, orders, order_items, order_item_refunds = load_data()
 
-total_session=count(website_sessions['website_sessions_id'])
+total_session=website_sessions['website_sessions_id'].nunique()
 
 col1, = st.columns(1)
 col1.metric("Total Sessions", total_sessions)
