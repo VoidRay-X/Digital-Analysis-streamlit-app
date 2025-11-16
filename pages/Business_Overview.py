9978c79358dcdc3a27ce2a097ed4de31e2ea0290
VoidRay-X <anmolkm18@gmail.com>
2025-11-16 18:38:40 +0530

    Update Business_Overview.py

--- pages/Business_Overview.py
+++ pages/Business_Overview.py
@@ -1,1 +1,15 @@
+import streamlit as st
+from data_loader import load_data
+import plotly.express as px
 
+st.title("📈 Business Overview")
+
+# ----------------------------
+# LOAD DATA
+# ----------------------------
+website_sessions, website_pageviews, products, orders, order_items, order_item_refunds = load_data()
+
+total_session=count(website_sessions['website_sessions_id'])
+
+col1, = st.columns(1)
+col1.metric("Total Sessions", total_sessions)
